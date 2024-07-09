89d96e08e28d1cc7e6ca13dac5bc748be63c8596
sohyun329 <58158103+sohyun329@users.noreply.github.com>
2024-07-09 20:30:39 +0900

    Add files via upload
    streamlit application without OpenAI API Key

---
+++ 07-chatbot3.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+import os
+
+# ChatOpenAI 모델 초기화
+# chat = ChatOpenAI(temperature=0)
+
+# Streamlit UI 설정
+st.set_page_config(page_title="ChatOpenAI Demo", page_icon=":robot:")
+st.header('💬 ChatOpenAI Demo')
+
+# 사이드바에 OpenAI API 키 입력 필드 생성
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+if openai_api_key:
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+else:
+    st.info("Please add your Open API key to continue.")
+    st.stop()
+
+# OpenAI 클라이언트 초기화
+chat = ChatOpenAI(api_key=openai_api_key, temperature=0)
+
+# 세션 상태 초기화
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+
+# 대화 히스토리 표시
+for message in st.session_state.messages:
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+# 사용자 입력 처리
+if prompt := st.chat_input("무엇을 도와드릴까요?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    # OpenAI 클라이언트 초기화
+    #chat = ChatOpenAI(api_key=openai_api_key, temperature=0)
+    
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in chat.stream(st.session_state.messages):
+            full_response += (response.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+# 스크롤을 최하단으로 이동
+st.empty()
